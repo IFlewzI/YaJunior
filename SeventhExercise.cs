a4e6249d2a144d68d78bbc1182a0c4984d9ce658
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-05-17 00:02:56 +0300

    Update SeventhExercise.cs

--- SeventhExercise.cs
+++ SeventhExercise.cs
@@ -6,18 +6,17 @@ namespace idk_why__it_s_just_existing
     {
         static void Main()
         {
+            Console.Write("Вы приходите в больницу и перед вами очередь к врачу. Сколько старушек вы видите? ");
+            
+            int grannysCount = Convert.ToInt32(Console.ReadLine());
             int hourDuration = 60;
             int waitingTimeForOneGranny = 10;
-            int overallWaitingTime;
+            int overallWaitingTime = grannysCount * waitingTimeForOneGranny;
             int waitingHours;
             int waitingMinutes;
-            int grannysCount;
 
-            Console.Write("Вы приходите в больницу и перед вами очередь к врачу. Сколько старушек вы видите? ");
-            grannysCount = Convert.ToInt32(Console.ReadLine());
-
-            waitingHours = grannysCount * waitingTimeForOneGranny / hourDuration;
-            waitingMinutes = grannysCount * waitingTimeForOneGranny % hourDuration;
+            waitingHours = overallWaitingTime / hourDuration;
+            waitingMinutes = overallWaitingTime % hourDuration;
 
             Console.WriteLine("Для очереди в {0} бабуль, вам необходимо прождать {1} часов и {2} минут", grannysCount, waitingHours, waitingMinutes);
         }

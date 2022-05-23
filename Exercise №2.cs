7afe85b01245cf591436372b2ea95a280905dc02
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-05-23 22:48:12 +0300

    Create Exercise №2.cs

---
+++ Exercise №2.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace idk_why__it_s_just_existing
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput;
+
+            do
+            {
+                Console.Write("Введите текст: ");
+                userInput = Console.ReadLine();
+            } while ((userInput != "exit") && (userInput != "Exit"));
+        }
+    }
+}

735eed383cea15e99f2fd62dd9ac75c4832ccd1b
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-05-30 14:05:34 +0300

    Update Exercise №5 (another one try).cs

--- Exercise №5 (another one try).cs
+++ Exercise №5 (another one try).cs
@@ -10,20 +10,35 @@ namespace idk_why__it_s_just_existing
             float rub = 500;
             float howMuchMoneyToConvert = 10;
             float rateRubleToDollar = 56;
+            float rateDollarToRuble = 1 / rateRubleToDollar;
 
             Console.WriteLine("Ваш баланс: \nДоллары: {0} \nРубли: {1}", usd, rub);
             Console.WriteLine("\nПроисходит конвертация {0} долларов в рубли...", howMuchMoneyToConvert);
 
-            usd -= howMuchMoneyToConvert;
-            rub += howMuchMoneyToConvert * rateRubleToDollar;
+            if (usd - howMuchMoneyToConvert >= 0)
+            {
+                usd -= howMuchMoneyToConvert;
+                rub += howMuchMoneyToConvert * rateRubleToDollar;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств для конвертации.");
+            }
 
             Console.WriteLine("\nОбновлённый баланс: \nДоллары: {0} \nРубли: {1}", usd, rub);
 
             howMuchMoneyToConvert = 112;
             Console.WriteLine("\nА теперь происходит конвертация {0} рублей в доллары...", howMuchMoneyToConvert);
 
-            rub -= howMuchMoneyToConvert;
-            usd += howMuchMoneyToConvert / rateRubleToDollar;
+            if (rub - howMuchMoneyToConvert >= 0)
+            {
+                rub -= howMuchMoneyToConvert;
+                usd += howMuchMoneyToConvert * rateDollarToRuble;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств для конвертации.");
+            }
 
             Console.WriteLine("\nОбновлённый баланс: \nДоллары: {0} \nРубли: {1}", usd, rub);
         }

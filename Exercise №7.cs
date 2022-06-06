3afb53d363a68aa0297e4cd27d79f6808bc948c9
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-06 14:36:47 +0300

    Update Exercise №7.cs

--- Exercise №7.cs
+++ Exercise №7.cs
@@ -7,30 +7,21 @@ namespace idk_why__it_s_just_existing
         static void Main()
         {
             string userName;
+            string symbolsString = "";
             char strokeSymbol;
             int indent = 2;
-
-            void printStrokedName(string name, char symbol)
-            {
-                for (int i = 0; i < name.Length + indent; i++)
-                {
-                    Console.Write(symbol);
-                }
-
-                Console.WriteLine("\n{0}{1}{2}", symbol, name, symbol);
-
-                for (int i = 0; i < name.Length + indent; i++)
-                {
-                    Console.Write(symbol);
-                }
-            }
 
             Console.Write("Введите имя: ");
             userName = Console.ReadLine();
             Console.Write("Введите символ: ");
             strokeSymbol = Convert.ToChar(Console.ReadLine());
 
-            printStrokedName(userName, strokeSymbol);
+            for (int i = 0; i < userName.Length + indent; i++)
+            {
+                symbolsString += strokeSymbol;
+            }
+
+            Console.WriteLine("{0} \n{1}{2}{1} \n{0}", symbolsString, strokeSymbol, userName);
         }
     }
 }

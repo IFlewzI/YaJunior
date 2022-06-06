597275945158bbdf9e64fd3eec201dd80f83b083
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-06 14:53:10 +0300

    Update Exercise №7.cs

--- Exercise №7.cs
+++ Exercise №7.cs
@@ -8,20 +8,20 @@ namespace idk_why__it_s_just_existing
         {
             string userName;
             string symbolsString = "";
-            char strokeSymbol;
+            char outlineSymbol;
             int indent = 2;
 
             Console.Write("Введите имя: ");
             userName = Console.ReadLine();
             Console.Write("Введите символ: ");
-            strokeSymbol = Convert.ToChar(Console.ReadLine());
+            outlineSymbol = Convert.ToChar(Console.ReadLine());
 
             for (int i = 0; i < userName.Length + indent; i++)
             {
-                symbolsString += strokeSymbol;
+                symbolsString += outlineSymbol;
             }
 
-            Console.WriteLine("{0} \n{1}{2}{1} \n{0}", symbolsString, strokeSymbol, userName);
+            Console.WriteLine("{0} \n{1}{2}{1} \n{0}", symbolsString, outlineSymbol, userName);
         }
     }
 }

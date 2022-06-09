8306001c5be2adfb58314f394d8f44caa649bc92
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-09 14:23:44 +0300

    Update Exercise №9.cs

--- Exercise №9.cs
+++ Exercise №9.cs
@@ -7,18 +7,19 @@ namespace idk_why__it_s_just_existing
         static void Main()
         {
             Random random = new Random();
-            int N = random.Next(1, 27 + 1);
+            int numberForWhichWeSearchingMultiplesNumbers = random.Next(1, 27 + 1);
             int quanityOfMultiplesNumbers = 0;
+            int requiredLengthOfMultilesNumbers = 3;
 
-            Console.WriteLine("Изначальное число: {0}", N);
+            Console.WriteLine("Изначальное число: {0}", numberForWhichWeSearchingMultiplesNumbers);
 
-            for (int i = N; Convert.ToString(i).Length < 4; i += N)
+            for (int i = numberForWhichWeSearchingMultiplesNumbers; Convert.ToString(i).Length <= requiredLengthOfMultilesNumbers; i += numberForWhichWeSearchingMultiplesNumbers)
             {
-                if (Convert.ToString(i).Length == 3)
+                if (Convert.ToString(i).Length == requiredLengthOfMultilesNumbers)
                     quanityOfMultiplesNumbers++;
             }
 
-            Console.WriteLine("Количество трёхзначиных чисел, которые кратны {0}: {1}", N, quanityOfMultiplesNumbers);
+            Console.WriteLine("Количество трёхзначных чисел, которые кратны {0}: {1}", numberForWhichWeSearchingMultiplesNumbers, quanityOfMultiplesNumbers);
         }
     }
 }

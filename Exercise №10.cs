4194e586809a5be4816aea4a63b3c89fb0fba28b
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-09 14:01:02 +0300

    Create Exercise №10.cs

---
+++ Exercise №10.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace idk_why__it_s_just_existing
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int requiredNumber = random.Next(0, 100000);
+            int numberThatRaisingToAPower = 2;
+            int requiredDegree = 0;
+            int finalNumber = 0;
+
+            while (finalNumber < requiredNumber)
+            {
+                requiredDegree++;
+                finalNumber = Convert.ToInt32(Math.Pow(numberThatRaisingToAPower, requiredDegree));
+            }
+
+            Console.WriteLine("Чтобы получить число, которое будет больше {0}, нужно возвести {1} в степень {2} и получить число {3}. ", 
+                requiredNumber, numberThatRaisingToAPower, requiredDegree, finalNumber);
+        }
+    }
+}

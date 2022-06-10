a674ad7b594465ce0f37e68d1ed9c664e3d96191
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-10 14:11:22 +0300

    Update Exercise №10.cs

--- Exercise №10.cs
+++ Exercise №10.cs
@@ -12,13 +12,13 @@ namespace idk_why__it_s_just_existing
             int requiredDegree = 0;
             int finalNumber = 0;
 
-            while (finalNumber < requiredNumber)
+            for (int i = 1; finalNumber < requiredNumber; i++)
             {
-                requiredDegree++;
+                requiredDegree = i;
                 finalNumber = Convert.ToInt32(Math.Pow(numberThatRaisingToAPower, requiredDegree));
             }
 
-            Console.WriteLine("Чтобы получить число, которое будет больше {0}, нужно возвести {1} в степень {2} и получить число {3}. ", 
+            Console.WriteLine("Чтобы получить число, которое будет больше {0}, нужно возвести {1} в степень {2} и получить число {3}. ",
                 requiredNumber, numberThatRaisingToAPower, requiredDegree, finalNumber);
         }
     }

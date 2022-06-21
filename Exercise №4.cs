bb951e3fb0654b4b0b78deb1e042c0488fe7cf2f
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-21 22:11:17 +0300

    Create Exercise №4.cs

---
+++ Exercise №4.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace idk_why__it_s_just_existing
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isProgramRunning = true;
+            string userInput;
+            float[] numbers = new float[1];
+            float[] tempNumbers;
+            float sumOfNumbers = 0;
+
+            while (isProgramRunning)
+            {
+                Console.Write("\nВведите число или команду exit/sum. Поле для ввода: ");
+                userInput = Console.ReadLine();
+
+                switch (userInput.ToLower())
+                {
+                    case "sum":
+                        sumOfNumbers = 0;
+                        for (int j = 0; j < numbers.Length; j++)
+                            sumOfNumbers += numbers[j];
+
+                        Console.WriteLine("\nСумма всех введённых чисел равна: {0}", sumOfNumbers);
+                        break;
+                    case "exit":
+                        isProgramRunning = false;
+                        break;
+                    default:
+                        numbers[numbers.Length - 1] = Convert.ToInt32(userInput);
+                        tempNumbers = new float[numbers.Length + 1];
+                        
+                        for (int j = 0; j < numbers.Length; j++)
+                            tempNumbers[j] = numbers[j];
+
+                        numbers = tempNumbers;
+                        break;
+                }
+            }
+        }
+    }
+}

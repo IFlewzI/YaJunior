a920a6992c7702b33e396dbaef324519daaffeba
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-06-21 21:52:38 +0300

    Update Exercise №3.cs

--- Exercise №3.cs
+++ Exercise №3.cs
@@ -7,33 +7,28 @@ namespace idk_why__it_s_just_existing
         static void Main()
         {
             Random random = new Random();
+            int minRandomNumber = 0;
+            int maxRandomNumber = 9;
             int[] numbers = new int[30];
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = random.Next(0, 10);
+                numbers[i] = random.Next(minRandomNumber, maxRandomNumber + 1);
                 Console.Write(numbers[i] + " ");
             }
 
             Console.WriteLine("\n\nЛокальные максимумы: ");
 
-            for (int i = 0; i < numbers.Length; i++)
+            if (numbers[0] > numbers[1])
+                Console.Write(numbers[0] + " ");
+
+            if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
+                Console.Write(numbers[numbers.Length - 1] + " ");
+
+            for (int i = 1; i < numbers.Length - 1; i++)
             {
-                if (i == 0)
-                {
-                    if (numbers[i] > numbers[i + 1])
-                        Console.Write(numbers[i] + " ");
-                }
-                else if (i == numbers.Length - 1)
-                {
-                    if (numbers[i] > numbers[i - 1])
-                        Console.Write(numbers[i] + " ");
-                }
-                else
-                {
-                    if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
-                        Console.Write(numbers[i] + " ");
-                }
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                    Console.Write(numbers[i] + " ");
             }
         }
     }

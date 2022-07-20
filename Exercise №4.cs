65dee3a6ac65ac20b0bc5a8b72bec5be6b5ec2ac
Flewz <101345625+IFlewzI@users.noreply.github.com>
2022-07-20 16:08:44 +0300

    Create Exercise №4.cs

---
+++ Exercise №4.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace idk_why__it_s_just_existing
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isProgramRunning = true;
+            Dictionary<string, string> dossiers = new Dictionary<string, string>();
+            string userInput;
+
+            while (isProgramRunning)
+            {
+                Console.Write("Введите номер нужной команды: " +
+                    "\n1)Добавить досье" +
+                    "\n2)Вывести все досье" +
+                    "\n3)Удалить досье" +
+                    "\n4)Выход" +
+                    "\n\nПоле для ввода: ");
+
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        AddDossier(ref dossiers);
+                        break;
+                    case "2":
+                        PrintAllDossiers(dossiers);
+                        break;
+                    case "3":
+                        DeleteDossier(ref dossiers);
+                        break;
+                    case "4":
+                        isProgramRunning = false;
+                        break;
+                }
+
+                Console.WriteLine("\nНажмите любую клавишу чтобы продолжить...\n");
+                Console.ReadKey();
+
+                if (userInput != "2")
+                    Console.Clear();
+            }
+        }
+
+        static void AddDossier(ref Dictionary<string, string> dossiers)
+        {
+            string fullName;
+            string profession;
+
+            Console.Write("\nВведите ФИО. Поле для ввода: ");
+            fullName = Console.ReadLine();
+
+            Console.Write("\nВведите должность. Поле для ввода: ");
+            profession = Console.ReadLine();
+
+            dossiers.Add(fullName, profession);
+        }
+
+        static void PrintAllDossiers(Dictionary<string, string> dossiers)
+        {
+            if (dossiers.Count > 0)
+            {
+                string[] keysArray = dossiers.Keys.ToArray();
+                string[] valuesArray = dossiers.Values.ToArray();
+
+                for (int i = 0; i < dossiers.Count; i++)
+                {
+                    string[] fullName = keysArray[i].Split(' ');
+
+                    Console.WriteLine();
+
+                    for (int j = 0; j < fullName.Length; j++)
+                        Console.Write(fullName[j] + '-');
+
+                    Console.Write(valuesArray[i]);
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nВ списке нет ни одного досье");
+            }
+        }
+
+        static void DeleteDossier(ref Dictionary<string, string> dossiers)
+        {
+            string fullName;
+
+            Console.Write("\nВведите ФИО для последующего удаления. Поле для ввода: ");
+            fullName = Console.ReadLine();
+
+            if (dossiers.ContainsKey(fullName))
+            {
+                dossiers.Remove(fullName);
+                Console.WriteLine("\nДосье успешно удалено");
+            }
+            else
+                Console.WriteLine("\nДосье с таким ФИО не найдено.");
+        }
+    }
+}
